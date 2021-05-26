a520c2028298604316061fc2e8f21a04c3919508
Mycken <m0122333@gmail.com>
2021-05-26 20:38:47 +0300

    1

--- src/main/java/JM/exe/jmexe/JmexeApplication.java
+++ src/main/java/JM/exe/jmexe/JmexeApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -19,48 +20,44 @@ public class JmexeApplication {
 		SpringApplication.run(JmexeApplication.class, args);
 
 		RestTemplate restTemplate = new RestTemplate();
-		String cookie;
 
 //	Запрос на получение всех юзеров
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<String> entity = new HttpEntity<>(headers);
 		ResponseEntity<String> response = restTemplate.exchange("http://91.241.64.178:7081/api/users", HttpMethod.GET, entity, String.class);
+		String cookie = response.getHeaders().get("Set-Cookie").toString().substring(1,62);
+		System.out.println();
 		System.out.print("Заголовок Set-Cookie: ");// Определяем значение заголовка Set-Cookie
-		System.out.println(cookie = response.getHeaders().get("Set-Cookie").toString().substring(1,62));//
+		System.out.print("as string: "+cookie);
 
 //	Добаление нового юзера
 		User user = new User(3L,"James","Brown",(byte)50);
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		headers.set("Set-Cookie",cookie);
-		HttpEntity<User> entity1 = new HttpEntity<>(user,headers);
-		System.out.println("Запрос на добавление = user+header: " +  entity1);
+		headers.add("Cookie",cookie);
+		HttpEntity<User> entity_add = new HttpEntity<>(user,headers);
+		System.out.println("Запрос на добавление = user+header: " +  entity_add);
 
-		ResponseEntity<String> response1 = restTemplate.exchange("http://91.241.64.178:7081/api/users", HttpMethod.POST, entity1, String.class);
+		ResponseEntity<String> response_add = restTemplate.exchange("http://91.241.64.178:7081/api/users", HttpMethod.POST, entity_add, String.class);
 		//Получаем первую часть кода  5ebfeb
-		System.out.println("Первая часть кода: "+response1.getBody());
+		System.out.println("Первая часть кода: "+response_add.getBody());
 
 //  Запрос на изменение юзера
 		user.setName("Thomas");
 		user.setLastName("Shelby");
-		HttpEntity<User> entity2 = new HttpEntity<>(user,headers);
-		System.out.println("Запрос на изменение = user+header: " +  entity2);
+		HttpEntity<User> entity_up = new HttpEntity<>(user,headers);
+		System.out.println("Запрос на изменение = user+header: " +  entity_up);
 
-		ResponseEntity<String> response2 = restTemplate.exchange("http://91.241.64.178:7081/api/users/", HttpMethod.POST, entity2, String.class);
-		// Получаем вторую часть кода cea2a2
-		System.out.println("Вторая часть кода: " + response2.getBody());
+		ResponseEntity<String> response_up = restTemplate.exchange("http://91.241.64.178:7081/api/users/", HttpMethod.PUT, entity_up, String.class);
+		// Получаем вторую часть кода e7cb97
+		System.out.println("Вторая часть кода: " + response_up.getBody());
 
 //  Запрос на удаление юзера
 
-//		Map< String, String > params = new HashMap< String, String >();
-//		params.put("?id", "3");
-//		HttpEntity<Map> entity3 = new HttpEntity<Map>(params,headers);
-		System.out.println("Запрос на удаление = header: " +  entity2);
-
-		ResponseEntity<String> response3 = restTemplate.exchange("http://91.241.64.178:7081/api/users/3", HttpMethod.DELETE, entity, String.class);
-
-// Получаем третью часть кода. Зависит от цифры в url: /1 - dbadfc если /2 - bbd25d /3 - ошибка
-		System.out.println("Третья часть кода: " + response3.getBody());
+		System.out.println("Запрос на удаление = header: " +  entity_up);
+		ResponseEntity<String> response_del = restTemplate.exchange("http://91.241.64.178:7081/api/users/3", HttpMethod.DELETE, entity_up, String.class);
+// Получаем третью часть кода. 5dfcf9
+		System.out.println("Третья часть кода: " + response_del.getBody());
 	}
 
 	@Bean
